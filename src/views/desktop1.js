143e9167bfbde81b328aebd1f732fe9b53b9c321
TusiimeAllan <allantusiime@gmail.com>
2022-08-10 16:09:16 +0300

    Switching to code from Figma

---
+++ src/views/desktop1.js
@@ -0,0 +1,226 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './desktop1.css'
+
+const Desktop1 = (props) => {
+  return (
+    <div className="desktop1-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="desktop1-desktop1">
+        <img
+          src="/playground_assets/rectangle16%3A6-eseq-1500w.png"
+          alt="Rectangle16:6"
+          className="desktop1-rectangle1"
+        />
+        <img
+          src="/playground_assets/acelogo416%3A5-2f3n-200h.png"
+          alt="ACElogo416:5"
+          className="desktop1-a-c-elogo41"
+        />
+        <img
+          src="/playground_assets/polygon16%3A7-o5ld.svg"
+          alt="Polygon16:7"
+          className="desktop1-polygon1"
+        />
+        <div className="desktop1-frame1">
+          <div className="desktop1-group1">
+            <img
+              src="/playground_assets/ellipse18%3A19-i19j-200h.png"
+              alt="Ellipse18:19"
+              className="desktop1-ellipse1"
+            />
+            <img
+              src="/playground_assets/simplify18%3A18-qr6n-200h.png"
+              alt="simplify18:18"
+              className="desktop1-simplify1"
+            />
+          </div>
+          <span className="desktop1-text">
+            <span>Simplify Content Creation</span>
+          </span>
+          <span className="desktop1-text02">
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </span>
+          </span>
+        </div>
+        <div className="desktop1-frame2">
+          <div className="desktop1-group11">
+            <img
+              src="/playground_assets/ellipse18%3A26-wlyya-200h.png"
+              alt="Ellipse18:26"
+              className="desktop1-ellipse11"
+            />
+            <img
+              src="/playground_assets/helpinghand18%3A44-v9ki-200h.png"
+              alt="helpinghand18:44"
+              className="desktop1-helpinghand1"
+            />
+          </div>
+          <span className="desktop1-text04">
+            <span>Avail 3D Models to Teams</span>
+          </span>
+          <span className="desktop1-text06">
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </span>
+          </span>
+        </div>
+        <div className="desktop1-frame3">
+          <div className="desktop1-group12">
+            <img
+              src="/playground_assets/ellipse18%3A38-g59b-200h.png"
+              alt="Ellipse18:38"
+              className="desktop1-ellipse12"
+            />
+            <img
+              src="/playground_assets/image3dprinter18%3A45-uvk9-200h.png"
+              alt="IMAGE3dprinter18:45"
+              className="desktop1-i-m-a-g-e3dprinter1"
+            />
+          </div>
+          <span className="desktop1-text08">
+            <span>3D Printing Ready</span>
+          </span>
+          <span className="desktop1-text10">
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </span>
+          </span>
+        </div>
+        <span className="desktop1-text12">
+          <span>
+            <span>Content Creation</span>
+            <br></br>
+            <span>made easy</span>
+          </span>
+        </span>
+        <span className="desktop1-text17">
+          <span>By The ACE VR Work Group</span>
+        </span>
+        <span className="desktop1-text19">
+          <span>Browse through our 3D Models</span>
+        </span>
+        <img
+          src="/playground_assets/line18%3A49-n3d.svg"
+          alt="Line18:49"
+          className="desktop1-line1"
+        />
+        <div className="desktop1-frame4">
+          <img
+            src="/playground_assets/rectangle28%3A50-l9-400h.png"
+            alt="Rectangle28:50"
+            className="desktop1-rectangle2"
+          />
+          <span className="desktop1-text21">
+            <span>Model Title</span>
+          </span>
+        </div>
+        <div className="desktop1-frame7">
+          <img
+            src="/playground_assets/rectangle28%3A60-cj7-400h.png"
+            alt="Rectangle28:60"
+            className="desktop1-rectangle21"
+          />
+          <span className="desktop1-text23">
+            <span>Model Title</span>
+          </span>
+        </div>
+        <div className="desktop1-frame5">
+          <img
+            src="/playground_assets/rectangle28%3A54-n4vc-400h.png"
+            alt="Rectangle28:54"
+            className="desktop1-rectangle22"
+          />
+          <span className="desktop1-text25">
+            <span>Model Title</span>
+          </span>
+        </div>
+        <div className="desktop1-frame8">
+          <img
+            src="/playground_assets/rectangle28%3A63-2fd1-400h.png"
+            alt="Rectangle28:63"
+            className="desktop1-rectangle23"
+          />
+          <span className="desktop1-text27">
+            <span>Model Title</span>
+          </span>
+        </div>
+        <div className="desktop1-frame6">
+          <img
+            src="/playground_assets/rectangle28%3A57-7tl-400h.png"
+            alt="Rectangle28:57"
+            className="desktop1-rectangle24"
+          />
+          <span className="desktop1-text29">
+            <span>Model Title</span>
+          </span>
+        </div>
+        <div className="desktop1-frame9">
+          <img
+            src="/playground_assets/rectangle28%3A66-6w76-400h.png"
+            alt="Rectangle28:66"
+            className="desktop1-rectangle25"
+          />
+          <span className="desktop1-text31">
+            <span>Model Title</span>
+          </span>
+        </div>
+      </div>
+      <footer className="desktop1-footer">
+        <div className="desktop1-container1">
+          <img
+            alt="logo"
+            src="https://presentation-website-assets.teleporthq.io/logos/logo.png"
+            className="desktop1-image"
+          />
+          <nav className="desktop1-nav">
+            <span className="desktop1-text33">About</span>
+            <span className="desktop1-text34">Features</span>
+            <span className="desktop1-text35">Pricing</span>
+            <span className="desktop1-text36">Team</span>
+            <span className="desktop1-text37">Blog</span>
+          </nav>
+        </div>
+        <div className="desktop1-separator"></div>
+        <div className="desktop1-container2">
+          <span className="desktop1-text38">
+            © 2021 teleportHQ, All Rights Reserved.
+          </span>
+          <div className="desktop1-icon-group">
+            <svg viewBox="0 0 950.8571428571428 1024" className="desktop1-icon">
+              <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+            </svg>
+            <svg
+              viewBox="0 0 877.7142857142857 1024"
+              className="desktop1-icon2"
+            >
+              <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+            </svg>
+            <svg
+              viewBox="0 0 602.2582857142856 1024"
+              className="desktop1-icon4"
+            >
+              <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+            </svg>
+          </div>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default Desktop1
